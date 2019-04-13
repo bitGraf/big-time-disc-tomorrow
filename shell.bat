ba853d8a01544f535b23eb3b0503dacc3a5e1167
bitGraf <ablasko27@gmail.com>
2019-04-12 23:05:28 -0500

    setup cl compiler

---
+++ shell.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+rem set path=g:\run_tree\;g:\misc\;%path%
+REM set include=g:\code\;%include%
+REM set libpath=g:\deps\libs\%libpath%
