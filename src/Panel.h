44304169f5b823d0cb81de589925aa10264aa155
Andrew B <ablaskov27@gmail.com>
2019-04-20 23:56:37 -0500

    Start working on panel size

--- src/Panel.h
+++ src/Panel.h
@@ -7,9 +7,22 @@
 
 struct PanelEnt : EntityBase {
     float d = realmax;
+    float length = 5;
+    float width = 3;
+
     float distanceToPoint(vec3 Py) {
         vec3 r = Py - position;
         d = abs(Vector::dot(r, Up));
+
+        vec3 pRedDot = Py - Up*d;
+        vec3 u_worldSpace = pRedDot - position;
+        quat invOrientation = 
+            {-orientation.x, -orientation.y, -orientation.z, orientation.w};
+        vec3 u = Quaternion::transformVector(invOrientation, u_worldSpace);
+
+        if (abs(u.x) > length || abs(u.z) > width) {
+            //The red dot is not inside the panel.
+        }
 
         return d;
     }
